87b922a8df28037f017617e948867d56a4b627af
Oliver Spindler <os@oliverspindler.com>
2013-10-09 18:04:28 +0100

    Change jquery cdn location

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ require.config({
   },
   
   paths: {
-    jquery: "http://code.jquery.com/jquery-2.0.0.min",
+    jquery: "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min",
     jquerymockjax: "//cdnjs.cloudflare.com/ajax/libs/jquery-mockjax/1.5.1/jquery.mockjax",
     'jquery-nestable': './lib/jquery.nestable',
     underscore: "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min",
