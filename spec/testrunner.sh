83d9decf05076b60e599a7ea16afd996363f3775
Oliver Spindler <os@oliverspindler.com>
2013-10-09 23:51:24 +0100

    Fix phantomjs testrunner

--- spec/testrunner.sh
+++ spec/testrunner.sh
@@ -1,5 +1,5 @@
-grunt server &
+node app.js &
 SERVER=$!
 SPECDIR="$( cd "$(dirname "$0")" ; pwd -P )"
-phantomjs $SPECDIR/lib/phantomjs-testrunner.js "http://localhost:8000/spec/index-ci.html"
+phantomjs $SPECDIR/lib/phantomjs-testrunner.js "http://localhost:3000/spec/index-ci.html"
 kill $SERVER
